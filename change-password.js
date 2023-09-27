0fa32e7c08c440c3b0aa1379837321816ede136f
Subhadip Hazra <129437065+Thanos123456790@users.noreply.github.com>
2023-09-27 10:10:30 +0530

    Add files via upload

---
+++ change-password.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const changePasswordForm = document.getElementById("change-password-form");
+
+    if (changePasswordForm) {
+        changePasswordForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            const username = document.getElementById("username").value;
+            const email = document.getElementById("email").value; // Retrieve email from the hidden field
+            const newPassword = document.getElementById("new-password").value;
+
+            // Send a request to update the password
+            fetch("http://localhost:5500/change-password", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, email, newPassword }),
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((passwordChangeResult) => {
+                if (passwordChangeResult.success) {
+                    // Password changed successfully, you can redirect to a login page or display a success message
+                    alert("Password changed successfully.");
+                    // Redirect to login page, assuming you have a login.html
+                    window.location.href = "login.html";
+                } else {
+                    // Password change failed, handle the error accordingly
+                    alert("Password change failed. Please try again.");
+                }
+            })
+            .catch((error) => {
+                console.error('Password Change Error:', error);
+                alert("An error occurred during password change.");
+            });
+        });
+    }
+});
---
+++ dislike.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sendFeedbackButton = document.getElementById("send-feedback");
+    const feedbackMessageInput = document.getElementById("feedback-message");
+    const feedbackPopup = document.getElementById("feedback-popup");
+    const closePopupButton = document.getElementById("close-popup");
+
+    sendFeedbackButton.addEventListener("click", function () {
+        const feedbackMessage = feedbackMessageInput.value;
+
+        // Send the feedback message to your backend
+        fetch("http://localhost:5500/send-feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: feedbackMessage,
+            }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                // Display the feedback popup
+                feedbackPopup.style.display = "flex";
+            } else {
+                alert("Failed to send feedback. Please try again later.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error sending feedback:", error);
+            alert("Failed to send feedback. Please try again later.");
+        });
+    });
+
+    // Close the feedback popup when the "Close" button is clicked
+    closePopupButton.addEventListener("click", function () {
+        feedbackPopup.style.display = "none";
+        feedbackMessageInput.value = ""; // Clear the input field
+    });
+});
